b8c6fcab9844e7b7cf8734b5fcfd0983fb73db47
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-03-29 11:47:38 +0200

    work in progress

--- README.md
+++ README.md
@@ -3,30 +3,32 @@ Create a movie database within design pattern.
 
 Link to GitHub: https://github.com/SvetlanaSS/Movie_database.
 
-File "movie_database.js" includes index.html and have functions to add and show movie.
+File "movie_database.js" contains several modules. First module ModuleUserInput has a simple interface. You can run html-file and create a new movie and see a list of movies. The other modules and functions in them can only be checked using the Console.
 
-File "assignment.js" does not contain an interface, only a simple HTML file for easy checking in the Console.
 
-File "assignment.js" contains the following structure:
-1) Movie(title, year, genres) - this is a special "constructor" function that works with objects. The function has a place in the global scope. Inside the "constructor", the keyword "this" refers to the newly created object.
-Calling the function with a prefix "new", I tell JS that this is the "constructor" function, it initializes a new object with the members defined in it. It means I can create new copies of the movies:
-film1 = new Movie("Remember me", 2011, ["Kids", "Drama", "Romance"], [4, 5, 9, 4]).
+File "movie_database.js" contains the following structure:
+1. **ModuleUserInput** - a module to take user input values from the form, create a new movie and show a list of the movies. Module can be checked using a html-file. The module contains the following functions:
+⋅⋅* **_processUserInput()_** - a function that takes user input values from the form,
+⋅⋅* getTitle() - a function to get film title from the form,
+⋅⋅* getYear() - a function to get year from the form,
+⋅⋅* getGenres() - a function to get selected genres from the form,
+⋅⋅* Movie(title, year, genres, ratings) - a special "constructor" function that works with objects,
+⋅⋅* createNewMovie(title, year, genres, ratings) - a function to create a new movie,
+⋅⋅* showListOfMovies(film) - a function to show a list of all movies.
 
-This is a simple version of the Constructor Pattern. With it it's easy to add new movies and it's understandable for me. But the version has some problems, one of them is the complexity with inheritance. But I have nothing to inherit in my task and so I ised it.
-
-2) ModuleRating - a module that is responsible for calculating the ratings of films,
-  * rateMovie() - a function that rates movie and returns a movie object,
-  * _getAverageRatedMovieArray - a private function starts with "_" that is not called by the user and is used to calculate the best and worst ratings of movies,
-  * getTopRatedMovie - a function that get top rated movie as an object,
-  * getWorstRatedMovie - a function that get worst rated movie as an object,
+2. **ModuleRating** - a module that is responsible for calculating the ratings of films,
+⋅⋅* rateMovie() - a function that rates movie and returns a movie object,
+⋅⋅* _getAverageRatedMovieArray - a private function starts with "_" that is not called by the user and is used to calculate the best and worst ratings of movies,
+⋅⋅* getTopRatedMovie - a function that get top rated movie as an object,
+⋅⋅* getWorstRatedMovie - a function that get worst rated movie as an object,
 
 3) ModuleYear - a module that is responsible to get a list of movies with the selected year,
-  * getMoviesThisYear - a function of the same value,
+⋅⋅* getMoviesThisYear - a function of the same value,
 
 4) ModuleGenres - a module that is responsible for working with genres,
-  * getMoviesByGenre - a function that get all movies of the same genres as the user inputs value,
-  * removeMovieGenres - a function that delete genre/genres from the film (delete user inputs value),
-  * addMovieGenres - a function that add genre/genres to the film (add user inputs value).
+⋅⋅* getMoviesByGenre - a function that get all movies of the same genres as the user inputs value,
+⋅⋅* removeMovieGenres - a function that delete genre/genres from the film (delete user inputs value),
+⋅⋅* addMovieGenres - a function that add genre/genres to the film (add user inputs value).
 
 Module Pattern isolates parts of the application logic from the global context to avoid conflicts and errors. Public variables and functions in a global context can conflict with interfaces of other developers or with libraries.
 Module pattern returns only the public part of the API, leaving everything else accessible only within the closures.
--- movie_database.js
+++ movie_database.js
@@ -1,86 +1,257 @@
-// This file includes HTML and have functions to add and show movie.
-// Take user input values from the form, create a new movie and show a list of our movies
-function processUserInput() {
-  // Take user input
-  let title = getTitle(),
-      year = getYear(),
-      genres = getGenres(); //return array of selected genres
-  // Create an object from user input
-  let film = createNewMovie(title, year, genres, ratings);
-  showListOfMovies(film);
-}
+/* First module ModuleUserInput has a simple interface. You can run html-file and create a new movie and see a list of movies. I use DOM-element and
+jquery's selectors.
+The other modules do not have an interface. Unfortunately, I did not have time to complete the interface. */
 
-// Get film title and clean user input afterwards
-function getTitle() {
-  let title = document.getElementById("title").value;
-  document.getElementById("title").value = "";
-  return title;
-}
 
-// Get year and clean user input afterwards
-function getYear(){
-  let year = document.getElementById("year").value;
-  document.getElementById("year").value = "";
-  return year;
-}
+/* Utilize Module Pattern. A Module to take user input values from the form, create a new movie and show a list of the movies.
+This module has html-file.*/
+let ModuleUserInput = (function(){
+  let processUserInput = function() {
+    // Take user input
+    let title = getTitle(),
+        year = getYear(),
+        genres = getGenres(); //return array of selected genres
+    // Create an object from user input
+    let film = createNewMovie(title, year, genres, ratings);
+    showListOfMovies(film);
+  };
 
-// Collect user selected genres and clean user input afterwards
-function getGenres() {
-  let genres = ["action", "adventure", "comedy", "crime", "drama", "fantasy",
-                "history", "horror", "mystery", "music", "romance", "thriller"],
-      selectedGenres = [];
-  genres.map(function(value) {
-    if (document.querySelector(`.${value}:checked`)) {
-      selectedGenres.push(value);
-      document.querySelector(`.${value}`).checked = false;
-    }
-  });
-  return selectedGenres;
-}
+  // Get film title and clean user input afterwards
+  let getTitle = function() {
+    let title = document.getElementById("title").value;
+    document.getElementById("title").value = "";
+    return title;
+  };
 
-// Create the Constructor Pattern
-function Movie(title, year, genres, ratings) {
-  this.title = title;
-  this.year = year;
-  this.genres = genres;
-  this.ratings = ratings;
-}
+  // Get year and clean user input afterwards
+  let getYear = function(){
+    let year = document.getElementById("year").value;
+    document.getElementById("year").value = "";
+    return year;
+  };
 
-// Now we can create the objects and push them to the movies
-function createNewMovie(title, year, genres, ratings) {
-  film = new Movie(title, year, genres, ratings);
-  console.log(film);
-  return film;
-}
+  // Collect user selected genres and clean user input afterwards
+  let getGenres = function() {
+    let genres = ["action", "adventure", "comedy", "crime", "drama", "fantasy",
+                  "history", "horror", "mystery", "music", "romance", "thriller"],
+        selectedGenres = [];
+    genres.map(function(value) {
+      if (document.querySelector(`.${value}:checked`)) {
+        selectedGenres.push(value);
+        document.querySelector(`.${value}`).checked = false;
+      }
+    });
+    return selectedGenres;
+  };
 
-// Show the list of all movies
-function showListOfMovies(film) {
-  let showAllMovies = document.getElementById("show-all-movies"),
-      line = document.createElement("tr"),
-      ratingForm = document.getElementById("ratings");
-  line.innerHTML =
-    `<td>${film.title}</td>
-     <td>${film.year}</td>
-     <td>${film.genres}</td>
-     <td></td>
-     <td>
-      <form>
-       ${ratingForm.innerHTML}
-       <button type="button" onclick="getRating(this.form)" value="Add">Add</button>
-       </form>
-     </td>`
-  showAllMovies.appendChild(line);
-}
+  // Create the Constructor Pattern
+  let Movie = function(title, year, genres, ratings) {
+    this.title = title;
+    this.year = year;
+    this.genres = genres;
+    this.ratings = ratings;
+  };
 
-// Collect user selected rating and clean user input afterwards
-function getRating(form) {
-  let selectedRating = [];
-  for (let i = 0; i < form.length; i++) {
-    if (form[i].checked) {
-      selectedRating.push(form[i].value);
-      form[i].checked = false;
-      return selectedRating;
-    }
+  // Now we can create the objects and push them to the movies
+  let createNewMovie = function(title, year, genres, ratings) {
+    film = new Movie(title, year, genres, ratings);
+    console.log(film);
+    return film;
+  };
+
+  // Show the list of all movies
+  let showListOfMovies = function(film) {
+    let showAllMovies = document.getElementById("show-all-movies"),
+        line = document.createElement("tr"),
+        ratingForm = document.getElementById("ratings");
+    line.innerHTML =
+      `<td>${film.title}</td>
+       <td>${film.year}</td>
+       <td>${film.genres}</td>
+       <td></td>
+       <td>
+        <form>
+         ${ratingForm.innerHTML}
+         <button type="button" onclick="getRating(this.form)" value="Add">Add</button>
+         </form>
+       </td>`
+    showAllMovies.appendChild(line);
+  };
+
+  return {
+    processUserInput: processUserInput,
+    getTitle: getTitle,
+    getYear: getYear,
+    getGenres:getGenres,
+    Movie: Movie,
+    createNewMovie: createNewMovie,
+    showListOfMovies:showListOfMovies
+  };
+
+})();
+
+
+/* I did not complete the interface and I can not give a rating to the movie in that interface. So I'm writing an example of the movie object
+to be able to test the functionality of other functions in the Console, when I start html-file */
+
+// Example of movies database
+let movies = [
+  {
+      title: "The Lobster",
+      year: 2015,
+      genres: ["Comedy", "Drama", "Romance", "Sci-Fi"],
+      ratings: [5, 4, 5, 5]
+  },
+  {
+      title: "The Ring",
+      year: 1854,
+      genres: ["Horror", "Thriller"],
+      ratings: [10, 10, 10, 10]
+  },
+  {
+      title: "The bla bla movie",
+      year: 2017,
+      genres: ["Comedy", "Thriller"],
+      ratings: [1, 1, 1, 0]
   }
-  return selectedRating;
-}
+]
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Utilize Module Pattern. This module is responsible for calculating the ratings of films.
+let ModuleRating = (function() {
+  // Rate movie function and return a movie
+  let rateMovie = function(movie, rating) { // rating array
+    movie.ratings = movie.ratings.concat(rating);
+    return movie;
+  }
+
+  // Private function _getAverageRatedMovieArray() is used by getWorstRatedMovie() and getTopRatedMovie() public functions
+	let _getAverageRatedMovieArray = function() {
+		resultsArray = [];
+		movies.map((movie) => {
+			let sum = movie.ratings.reduce(function(a, b) { return a + b; });
+			let ratingNumber = sum / movie.ratings.length;
+			resultsArray.push({ movie, ratingNumber })
+			return resultsArray;
+		})
+		return resultsArray;
+		/* Returns movies in the following format:
+		[{ movie: movieObject, ratingNumber: 4.75 }, { movie: movieObject, ratingNumber: 10 }] */
+	}
+
+	// Get top rated movie as an object
+	let getTopRatedMovie = function() {
+		let moviesWithRatings = _getAverageRatedMovieArray();
+		// Returns max rating value
+		let maxRes = Math.max.apply(Math, moviesWithRatings.map((object) => {
+			return object.ratingNumber;
+		}));
+		// Search movie with maximum rating value
+		let result = moviesWithRatings.find((object) => {
+			return object.ratingNumber == maxRes;
+		});
+		return result;
+	};
+
+	// Get worst rated movie as an object
+	let getWorstRatedMovie = function() {
+	  let moviesWithRatings = _getAverageRatedMovieArray();
+	  // Returns min rating value
+	  let minRes = Math.min.apply(Math, moviesWithRatings.map((object) => {
+	    return object.ratingNumber;
+	  }));
+	  // Search movie with minimun rating value
+	  let result = moviesWithRatings.find((object) => {
+	    return object.ratingNumber == minRes;
+	  });
+	  return result;
+	};
+
+  return {
+    rateMovie: rateMovie,
+    getTopRatedMovie: getTopRatedMovie,
+		getWorstRatedMovie: getWorstRatedMovie
+  };
+
+})();
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Utilize Module Pattern. This module is responsible to get a list of movies with the selected year.
+let ModuleYear = (function() {
+  let getMoviesThisYear = function(year) {
+    resultsArray = [];
+    movies.map((movie) => {
+      if (movie.year === year) {
+        resultsArray.push(movie);
+      }
+      return resultsArray;
+    });
+    return resultsArray;
+  };
+
+  return {
+    getMoviesThisYear: getMoviesThisYear
+  };
+
+})();
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Utilize Module Pattern. This module is responsible for working with genres.
+let ModuleGenres = (function() {
+  // Get all films of the same genre as the value of the parameter genres. There is a possibility to filter on multiple genres
+  let getMoviesByGenre = function(inputGenres) {
+    /* For example:
+    Movies array = ["Comedy", "Drama", "Romance", "Sci-Fi"], ["Horror", "Thriller"], ["Comedy", "Thriller"]
+    User input, inputGenres = ["Comedy", "Drama"].
+    I need to check if all values of a inputGenres included in a movies array*/
+    movies.map((movie) => {
+      let movieGenres = movie.genres;
+      let isarrayOfUserInputSubsetOfMovieGenres = inputGenres.every((val) => {
+        return movieGenres.indexOf(val) >= 0;
+      });
+      if (isarrayOfUserInputSubsetOfMovieGenres) {
+        console.log(movie.title);
+      };
+    });
+  };
+
+
+  // Remove genre/genres from the film
+  let removeMovieGenres = function(genres) {
+    /* For example:
+    User selects the movie "The Lobster" with the genres ["Comedy", "Drama", "Romance", "Sci-Fi"] and wants to delete
+    the following genres ["Romance", "Sci-Fi"]
+    It means to delete array from array: user input array (with one or several values) from movies array */
+    movies.map((movie) => {
+      let resultGenres = movie.genres.filter((elem) => {
+        return genres.indexOf(elem) === -1;
+      });
+      movie.genres = resultGenres;
+      console.log(movie.genres);
+    });
+  };
+
+
+  // Add genre/genres to the film
+  let addMovieGenres = function(inputGenres) {
+    /* For example:
+    User selects the movie "The Ring" with the genres ["Horror", "Thriller"] and wants to add
+    the following genres ["Mystic", "Sci-Fi"].
+    It meents to add array to array: user input array, inputGenres (with one or several values) to movies array.
+    But this function doesn't check if genre already exist */
+    movies.map((movie) => {
+      let resultGenres = movie.genres.concat(inputGenres);
+      movie.genres = resultGenres;
+      console.log(movie.genres);
+    })
+  };
+
+  return {
+    getMoviesByGenre: getMoviesByGenre,
+    removeMovieGenres: removeMovieGenres,
+    addMovieGenres: addMovieGenres
+  };
+
+})();
